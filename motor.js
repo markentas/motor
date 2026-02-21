61628ea25fec800c82b2b03993daf8cb7c3b4c52
Markentas <equipomarkentas@gmail.com>
2026-02-21 02:04:14 -0300

    feat: estructura inicial

--- motor.js
+++ motor.js
@@ -1,1 +1,7 @@
-
+console.log('Motor cargado');
+async function iniciar() {
+  const res = await fetch('./config.json');
+  const config = await res.json();
+  document.getElementById('app').innerHTML = '<h1>' + config.datos.titulo + '</h1>';
+}
+window.onload = iniciar;
